10c492738dafcd26a13b0a3e7b9f29dbfdb92034
Neopabo <152736148+Neopabo@users.noreply.github.com>
2024-02-07 13:40:22 -0500

    Updated LineEliminator.py
    Used list comprehension

--- LineEliminator.py
+++ LineEliminator.py
@@ -1,17 +1,15 @@
 #Line Eliminator
 
-File=open('FILE.txt') #Original Text File
+#Opens Text File
+File=open('FILE.txt')
 MyText=File.read()
 File.close()
 MyText=MyText.splitlines()
-MyTextNew=[]
 
-for i in MyText:
-    if len(i)>0:
-        MyTextNew.append(i)
+MyTextNew=[i for i in MyText if len(i)>0]
 
 MyTextNew2='\n'.join(MyTextNew)
 
-FileNew=open('FILE.txt', 'w') #New Text File
+FileNew=open('FILE.txt', 'w')
 FileNew.write(MyTextNew2)
 FileNew.close()
